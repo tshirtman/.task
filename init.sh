d4751b591b2bc956f4379f823295e1e7016d87ba
gabriel pettier <dev@tangibledisplay.com>
2015-08-04 11:49:20 +0200

    initial commit
    taskrc
    secret files in gpg encrypted format
    task-popup script
    init file to install everything

---
+++ init.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# run this script after cloning repo
+
+while true
+do
+read -p "do you want to build task with gnutls support? (Y/N)" yn
+case $yn in
+	[Yy]*)
+		sudo apt-get install libgnutls-dev uuid-dev -y
+		pushd ~/
+		wget http://taskwarrior.org/download/task-2.4.4.tar.gz
+		tar -xf task-2.4.4.tar.gz
+		cd task-2.4.4
+		cmake -DCMAKE_BUILD_TYPE=release .
+		make
+		ln -sf $PWD/src/task ~/.local/bin
+		popd
+		break;;
+	[Nn]*) break;;
+	*) echo "please answer yes or no";;
+esac
+done
+
+while true
+do
+read -p "do you want to link taskrc to ~/.taskrc (Y/N)" yn
+case $yn in
+	[Yy]*) ln -s $PWD/taskrc ~/.taskrc; break;;
+	[Nn]*) break;;
+	*) echo "please answer yes or no";;
+esac
+done
+
+while true
+do
+read -p "do you want to install popup crontab? (Y/N)" yn
+case $yn in
+	[Yy]*)
+		ln -sf $PWD/task-popup.sh ~/.local/bin/
+		(crontab -l; echo "*/30 * * * * task-popup.sh")|crontab -
+		break;;
+	[Nn]*) break;;
+	*) echo "please answer yes or no";;
+esac
+done
+
+while true
+do
+read -p "do you want to extract pem files? (Y/N)" yn
+case $yn in
+	[Yy]*)
+		gpg2 -d secret.tgz.gpg > secret.tgz
+		tar -xf secret.tgz
+		break;;
+	[Nn]*) break;;
+esac
+done
